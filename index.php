75904de044a5c3b4ee6bbb29ce0279b73dc462bd
Adria Blancafort <adriablancafort@gmail.com>
2024-02-21 09:58:21 +0100

    added type support

--- index.php
+++ index.php
@@ -11,14 +11,24 @@ if (isset($_SERVER['HTTP_ORIGIN']) && in_array($_SERVER['HTTP_ORIGIN'], $allowed
 }
 
 $url = $_GET['url'];
+$type = $_GET['type'];
 
 // Fetch the HTML content of the webpage
 $html = file_get_contents($url);
 
-// Check if 'wp-content' is present in the HTML
-if (strpos($html, 'wp-content') !== false) {
-    echo json_encode(['wp' => 'yes']);
-} else {
-    echo json_encode(['wp' => 'no']);
+if ($type === 'wp') {
+    // Check if 'wp-content' is present in the HTML
+    if (strpos($html, 'wp-content') !== false) {
+        echo json_encode(['wp' => 'yes']);
+    } else {
+        echo json_encode(['wp' => 'no']);
+    }
+} elseif ($type === 'themes') {
+    // Basic list of themes
+    $themes = ['theme1', 'theme2', 'theme3'];
+    echo json_encode(['themes' => $themes]);
+} elseif ($type === 'plugins') {
+    // Basic list of plugins
+    $plugins = ['plugin1', 'plugin2', 'plugin3'];
+    echo json_encode(['plugins' => $plugins]);
 }
-?>
