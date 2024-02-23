5166a646bad57ec2cb606e9393a8cc28978d968b
Adria Blancafort <adriablancafort@gmail.com>
2024-02-23 09:53:09 +0100

    request html as a mozilla  agent

--- index.php
+++ index.php
@@ -36,8 +36,47 @@ if ($type === 'wp') {
 }
 
 function get_html($url) {
-    // Returns the html content of the url
-    return file_get_contents($url);
+	$headers = array(
+	   "Connection: keep-alive",
+	   "Upgrade-Insecure-Requests: 1",
+	   "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+	   "Accept-Language: en,es-ES;q=0.9,es;q=0.8",
+	);
+	
+	//$agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36';
+	$agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/117.0';
+
+	$cookieFilePath = __DIR__ . '/cookie.txt';
+
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+	curl_setopt($ch, CURLOPT_VERBOSE, true);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($ch, CURLOPT_USERAGENT, $agent);
+	curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+	curl_setopt($ch, CURLOPT_ENCODING, '');
+	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+	curl_setopt($ch, CURLOPT_AUTOREFERER, true);
+	curl_setopt($ch, CURLOPT_CONNECTTIMEOUT , 60); 
+	curl_setopt($ch, CURLOPT_TIMEOUT, 60); 
+	curl_setopt($ch, CURLOPT_MAXREDIRS, 5); 
+	curl_setopt($ch, CURLOPT_COOKIE, 'consent=yes');
+	curl_setopt($ch, CURLOPT_COOKIEFILE, $cookieFilePath);
+	curl_setopt($ch, CURLOPT_COOKIEJAR, $cookieFilePath);
+	curl_setopt($ch, CURLOPT_URL,$url);
+	$result=curl_exec($ch);
+	
+	 // Check for cURL errors
+    if (curl_errno($ch)) {
+        $error_message = curl_error($ch);
+        error_log('cURL Error: ' . $error_message);
+        curl_close($ch);
+        return '';
+    }
+    
+	curl_close($ch);
+    
+	return $result;
 }
 
 function detect_wp($html) {
