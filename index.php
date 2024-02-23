df87d89423a0a9633d94ca5cd87bacc1a62f8f86
Adria Blancafort <adriablancafort@gmail.com>
2024-02-23 17:33:30 +0100

    updated functions

--- index.php
+++ index.php
@@ -13,25 +13,26 @@ if (isset($_SERVER['HTTP_ORIGIN']) && in_array($_SERVER['HTTP_ORIGIN'], $allowed
 $url = $_GET['url'];
 $type = $_GET['type'];
 $html = get_html($url);
+$wpContent = wp_content_path($html);
 
 if ($type === 'wp') {
-    $wp = detect_wp($html);
+    $wp = $wpContent !== false;
     echo json_encode(['wp' => $wp]);
 
 } elseif ($type === 'themes') {
-    $themes = detect_themes($html);
+    $themes = detect_themes($html, $wpContent);
     echo json_encode(['themes' => $themes]);
 
 } elseif ($type === 'plugins') {
-    $plugins = detect_plugins($html);
+    $plugins = detect_plugins($html, $wpContent);
     echo json_encode(['plugins' => $plugins]);
 
 } elseif ($type === 'top-themes') {
-    $themes = detect_themes($html);
+    $themes = top_themes();
     echo json_encode(['themes' => $themes]);
 
 } elseif ($type === 'top-plugins') {
-    $plugins = detect_plugins($html);
+    $plugins = top_plugins();
     echo json_encode(['plugins' => $plugins]);
 }
 
@@ -66,26 +67,96 @@ function get_html($url) {
 	curl_setopt($ch, CURLOPT_URL,$url);
 	$result=curl_exec($ch);
 	
-	 // Check for cURL errors
+	// Check for cURL errors
     if (curl_errno($ch)) {
         $error_message = curl_error($ch);
         error_log('cURL Error: ' . $error_message);
         curl_close($ch);
         return '';
     }
-    
+
 	curl_close($ch);
     
 	return $result;
 }
 
-function detect_wp($html) {
-    // Returns true if it finds the html content and false otherwise
-    return strpos($html, 'wp-content') !== false;
+function wp_content_path($html) {
+    
+    $dom = new DOMDocument();
+    libxml_use_internal_errors(true);
+    $dom->loadHTML($html); 
+    
+    $elements = array_merge(
+        iterator_to_array($dom->getElementsByTagName('link')),
+        iterator_to_array($dom->getElementsByTagName('a')),
+        iterator_to_array($dom->getElementsByTagName('img'))
+    );
+    
+    foreach ($elements as $link) {
+        $href = $link->getAttribute('href');
+        $src = $link->getAttribute('src');
+        
+        if (strpos($href, '/wp-content/') !== false) {
+            $parts = explode('wp-content/', $href);
+            $result = $parts[0] . 'wp-content/';
+            return $result;
+        }
+        
+        if (strpos($src, '/wp-content/') !== false) {
+            $parts = explode('wp-content/', $src);
+            $result = $parts[0] . 'wp-content/';
+            return $result;
+        }
+    }
+    
+    return false;
 }
 
-function detect_themes($html) {
-    // Returns a list of all the themes detected in the html content
+function detect_themes($html, $wpContent) {
+    // Returns a list of the themes detected in the html content
+    /*
+    Structure:
+
+    $theme1 = [
+        'banner' => 'https://generatepress.com/wp-content/themes/generatepress/screenshot.png',
+        'title' => 'GeneratePress',
+        'author' => 'Tom Usborne',
+        'version' => '3.4.0',
+        'website' => 'https://generatepress.com',
+        'sanatizedWebsite' => 'generatepress.com',
+        'reqWpVersion' => '5.2',
+        'testedWpVersion' => '6.3',
+        'reqPhpVersion' => '5.6',
+        'description' => 'GeneratePress is a lightweight WordPress theme built with a focus on speed and usability. Performance is important to us, which is why a fresh GeneratePress install adds less than 10kb (gzipped) to your page size. We take full advantage of the block editor (Gutenberg), which gives you more control over creating your content. If you use page builders, GeneratePress is the right theme for you. It is completely compatible with all major page builders, including Beaver Builder and Elementor. Thanks to our emphasis on WordPress coding standards, we can boast full compatibility with all well-coded plugins, including WooCommerce. GeneratePress is fully responsive, uses valid HTML/CSS, and is translated into over 25 languages by our amazing community of users. A few of our many features include 60+ color controls, powerful dynamic typography, 5 navigation locations, 5 sidebar layouts, dropdown menus (click or hover), and 9 widget areas. Learn more and check out our powerful premium version at generatepress.com',
+        'link' => 'https://generatepress.com/?utm_source=wp-detector',
+    ];
+    */
+}
+
+function detect_themes($html, $wpContent) {
+    // Returns a list of the plugins detected in the html content
+    /*
+    Structure:
+
+    $plugin1 = [
+        'banner' => 'https://ps.w.org/wordpress-seo/assets/banner-772x250.png',
+        'icon' => 'https://ps.w.org/wordpress-seo/assets/icon.svg',
+        'title' => 'Yoast SEO',
+        'author' => 'Team Yoast',
+        'version' => '3.4.0',
+        'website' => 'https://yoast.com',
+        'sanatizedWebsite' => 'yoast.com',
+        'reqWpVersion' => '6.3',
+        'testedWpVersion' => '6.4.3',
+        'reqPhpVersion' => '7.2.5',
+        'description' => 'Supercharge your website’s visibility and attract organic traffic with Yoast SEO, the WordPress SEO plugin trusted by millions worldwide. With those millions of users, we’ve definitely helped someone like you! Users of our plugin range from owners of small-town bakeries and local physical stores to some of the world’s largest and most influential organizations. And we’ve done this since 2008!',
+        'link' => 'https://yoast.com/?utm_source=wp-detector',
+    ];
+    */
+}
+
+function top_themes() {
+    // Returns a list of the most common themes
 
     $theme1 = [
         'banner' => 'https://generatepress.com/wp-content/themes/generatepress/screenshot.png',
@@ -104,8 +175,8 @@ function detect_themes($html) {
     return [$theme1];
 }
 
-function detect_plugins() {
-    // Returns a list of all the plugins detected in the html content
+function top_plugins() {
+    // Returns a list of the most common plugins
 
     $plugin1 = [
         'banner' => 'https://ps.w.org/wordpress-seo/assets/banner-772x250.png',
