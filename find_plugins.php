85570c75b4c2d4a94eaf7a2d45fed2017abb3f0d
Adria Blancafort <adriablancafort@gmail.com>
2024-04-19 23:50:03 +0200

    detect plugin icon and banner from wp public directory

--- find_plugins.php
+++ find_plugins.php
@@ -35,34 +35,36 @@ function find_plugin_info($pluginSlug, $pluginPath)
     $readmeTxtContent = get_content($readmeTxtUrl);
     
     preg_match('/=== (.*) ===/', $readmeTxtContent, $matches);
-    // Convert "plugin-slug" to "Plugin Slug"
-    $words = explode('-', $pluginSlug);
-    $words = array_map('ucfirst', $words);
-    $pluginSlug = implode(' ', $words);
-    $title = $matches[1] ?? $pluginSlug;
+    if (!isset($matches[1])) {
+        // Convert "plugin-slug" to "Plugin Slug"
+        $words = explode('-', $pluginSlug);
+        $words = array_map('ucfirst', $words);
+        $pluginTitle = implode(' ', $words);
+    }
+    $title = $matches[1] ?? $pluginTitle;
 
     preg_match('/Contributors: (.*)/', $readmeTxtContent, $matches);
-    $author = $matches[1] ?? '';
+    $author = $matches[1] ?? null;
 
     preg_match('/Stable tag: (.*)/', $readmeTxtContent, $matches);
-    $version = $matches[1] ?? '';
+    $version = $matches[1] ?? null;
 
     preg_match('/Donate link: (.*)/', $readmeTxtContent, $matches);
-    $website = $matches[1] ?? '';
+    $website = $matches[1] ?? null;
 
     $sanatizedWebsite = str_replace(['http://', 'https://'], '', $website);
 
     preg_match('/Requires at least: (.*)/', $readmeTxtContent, $matches);
-    $reqWpVersion = isset($matches[1]) ? $matches[1] . ' or higher' : 'Not specified';
+    $reqWpVersion = isset($matches[1]) ? $matches[1] . ' or higher' : null;
 
     preg_match('/Tested up to: (.*)/', $readmeTxtContent, $matches);
-    $testedWpVersion = $matches[1] ?? '';
+    $testedWpVersion = $matches[1] ?? null;
 
     preg_match('/Requires PHP: (.*)/', $readmeTxtContent, $matches);
-    $reqPhpVersion = isset($matches[1]) ? $matches[1] . ' or higher' : 'Not specified';
+    $reqPhpVersion = isset($matches[1]) ? $matches[1] . ' or higher' : null;
 
     preg_match('/== Description ==\n\n(.*)/', $readmeTxtContent, $matches);
-    $description = $matches[1] ?? '';
+    $description = $matches[1] ?? 'No description provided';
 
     $banner = get_plugin_banner($pluginSlug);
     $icon = get_plugin_icon($pluginSlug);
@@ -82,20 +84,50 @@ function find_plugin_info($pluginSlug, $pluginPath)
     ];
 
     return $plugin;
-
 }
 
 // Returns the banner URL of the plugin
 function get_plugin_banner($pluginSlug)
 {
-    $banner = 'https://ps.w.org/wordpress-seo/assets/banner-772x250.png';
-    return $banner;
+    $baseUrl = 'https://ps.w.org/' . $pluginSlug . '/assets/';
+
+    $bannerUrls = [
+        $baseUrl . 'banner-1544x500.png',
+        $baseUrl . 'banner-1544x500.jpg',
+        $baseUrl . 'banner-772x250.png',
+        $baseUrl . 'banner-772x250.jpg',
+    ];
+
+    foreach ($bannerUrls as $url) {
+        $headers = get_headers($url);
+        if ($headers && strpos($headers[0], '200') !== false) {
+            return $url;
+        }
+    }
+
+    return '/unknown-plugin-banner.webp';
 }
 
 // Returns the icon URL of the plugin
 function get_plugin_icon($pluginSlug)
 {
-    $icon = 'https://ps.w.org/wordpress-seo/assets/icon.svg';
-    return $icon;
+    $baseUrl = 'https://ps.w.org/' . $pluginSlug . '/assets/';
+
+    $bannerUrls = [
+        $baseUrl . 'icon.svg',
+        $baseUrl . 'icon-128x128.png',
+        $baseUrl . 'icon-128x128.gif',
+        $baseUrl . 'icon-256x256.png',
+        $baseUrl . 'icon-256x256.gif',
+    ];
+
+    foreach ($bannerUrls as $url) {
+        $headers = get_headers($url);
+        if ($headers && strpos($headers[0], '200') !== false) {
+            return $url;
+        }
+    }
+
+    return '/unknown-plugin-icon.webp';
 }
 ?>
