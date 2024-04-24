09073ef2a35d4e42d5d8e839a165bafa871e04fa
Adria Blancafort <adriablancafort@gmail.com>
2024-04-24 18:14:07 +0200

    delete unused functions

--- get_plugins.php
+++ get_plugins.php
@@ -1,41 +1,22 @@
 <?php
 
-// require_once 'open_database_connection.php';
+// require_once 'database_connection.php';
 
 // Returns the plugins of a given url
 function get_plugins($url)
 {
-    //$conn = open_database_connection();
-    //$plugins = database_read_plugins($conn, $url);
-    //if (empty($plugins)) {
+    //if (empty($row)) {
         require_once 'get_content.php';
         require_once 'find_links.php';
         require_once 'find_plugins.php';
         $html = get_content($url);
         $links = find_links($html);
         $plugins = find_plugins($links);
-        //database_write_plugins($conn, $url, $plugins);
     //}
-    //close_database_connection($conn);
 
     // Convert the associative array to an indexed array
     $plugins = array_values($plugins);
 
     return $plugins;
-}
-
-// Reads the plugins of a given url in the database
-function database_read_plugins($conn, $url)
-{
-    // Read all the plugin slugs of the website
-    // Make a joint with the table plugins to return all the pluginInfo for each one of the slugs
-    return null;
-}
-
-// Writes the plugins of a given url in the database
-function database_write_plugins($conn, $url, $wp)
-{
-    // Write a list of plugin slugs to the website in the websites table
-    return null;
 }
 ?>
--- get_themes.php
+++ get_themes.php
@@ -1,41 +1,22 @@
 <?php
 
-// require_once 'open_database_connection.php';
+// require_once 'database_connection.php';
 
 // Returns the plugins of a given url
 function get_themes($url)
 {
-    //$conn = open_database_connection();
-    //$themes = database_read_themes($conn, $url);
-    //if (empty($themes)) {
+    //if (empty($row)) {
         require_once 'get_content.php';
         require_once 'find_links.php';
         require_once 'find_themes.php';
         $html = get_content($url);
         $links = find_links($html);
         $themes = find_themes($links);
-        //database_write_themes($conn, $url, $themes);
     //}
-    //close_database_connection($conn);
 
     // Convert the associative array to an indexed array
     $themes = array_values($themes);
 
     return $themes;
-}
-
-// Reads the themes of a given url in the database
-function database_read_themes($conn, $url)
-{
-    // Read all the theme slugs of the website
-    // Make a joint with the table themes to return all the themeInfo for each one of the slugs
-    return null;
-}
-
-// Writes the themes of a given url in the database
-function database_write_themes($conn, $url, $wp)
-{
-    // Write a list of theme slugs to the website in the websites table
-    return null;
 }
 ?>
