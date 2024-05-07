58bfeb64c29d31fffb3e2d24189e5c9c426c2b9f
Adria Blancafort <adriablancafort@gmail.com>
2024-05-08 00:12:38 +0200

    read/write plugins from slugs in database

--- get_plugins.php
+++ get_plugins.php
@@ -1,11 +1,11 @@
 <?php
 
 require_once 'database_connection.php';
+require_once 'find_plugins.php';
 
 // Returns the plugins of a given url
 function get_plugins($url)
 {
-    /*
     $db = new Database();
     $db->connect();
 
@@ -14,7 +14,6 @@ function get_plugins($url)
     $row = $result->fetch_assoc();
 
     if (empty($row)) {
-    */
         require_once 'get_content.php';
         require_once 'find_links.php';
         require_once 'find_plugins.php';
@@ -22,52 +21,52 @@ function get_plugins($url)
         $links = find_links($html);
         $plugins = find_plugins($links);
 
-        // Convert the associative array to an indexed array
-        $plugins = array_values($plugins);
-
-    /*      
+        write_plugin_slugs_to_database($db, $plugins, $url);
+      
     } else {
-        $plugins = [];
-        $pluginSlugs = $row; // Correct to make an iterable array of slugs
+        // Split the string of plugin slugs into an array
+        $pluginSlugs = explode(',', $row['plugins']);
 
         foreach ($pluginSlugs as $pluginSlug) {
-
-            $result = $db->query("SELECT * FROM plugins WHERE slug = '$pluginSlug'");
-            $row = $result->fetch_assoc();
-
-            $pluginInfo = [
-                'banner' => $row['banner'],
-                'icon' => $row['icon'],
-                'title' => $row['title'],
-                'contributors' => $row['contributors'],
-                'version' => $row['version'],
-                'website' => $row['website'],
-                'sanatizedWebsite' => $row['sanatizedWebsite'],
-                'lastUpdated' => $row['lastUpdated'],
-                'activeInstallations' => $row['activeInstallations'],
-                'reqWpVersion' => $row['reqWpVersion'],
-                'testedWpVersion' => $row['testedWpVersion'],
-                'reqPhpVersion' => $row['reqPhpVersion'],
-                'description' => $row['description'],
-                'link' => $row['link'],
-            ];
-
+            $pluginSlug = trim($pluginSlug);
+            $pluginInfo = get_plugin_info($db, $pluginSlug, null);
             $plugins[$pluginSlug] = $pluginInfo;
         }
     }
 
     $db->close();
-    */
+
+    // Convert the associative array to an indexed array
+    $plugins = array_values($plugins);
 
     return $plugins;
 }
 
-function get_top_plugins()
+// Write plugins to database
+function write_plugin_slugs_to_database($db, $plugins, $url)
 {
+    // Convert the array of plugin slugs to a comma-separated string
+    $pluginSlugs = implode(',', array_keys($plugins));
+
+    $db->query("UPDATE websites SET plugins = '$pluginSlugs' WHERE url = '$url'");
+}
+
+function get_top_plugins($quantity, $page)
+{
+    // Sanitize and validate the quantity
+    $quantity = filter_var($quantity, FILTER_VALIDATE_INT);
+
+    if ($quantity === false) {
+        $quantity = 5;
+    }
+
+    // Calculate the offset
+    $offset = ($page - 1) * $quantity;
+
     $db = new Database();
     $db->connect();
 
-    $result = $db->query("SELECT * FROM plugins ORDER BY timesAnalyzed DESC LIMIT 5");
+    $result = $db->query("SELECT * FROM plugins ORDER BY timesAnalyzed DESC LIMIT $quantity OFFSET $offset");
 
     while ($row = $result->fetch_assoc()) {
         $pluginInfo = [
