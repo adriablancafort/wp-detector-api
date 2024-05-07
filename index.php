9d5d95894c083dcca957ff9ce1ae5f0a41b27597
Adria Blancafort <adriablancafort@gmail.com>
2024-05-07 23:56:11 +0200

    new url parameters structure

--- index.php
+++ index.php
@@ -7,27 +7,33 @@ require_once 'get_wp.php';
 require_once 'get_themes.php';
 require_once 'get_plugins.php';
 
-$url = $_GET['url'];
 $type = $_GET['type'];
 
 if ($type === 'wp') {
+    $url = $_GET['url'];
     $wp = get_wp($url);    
     echo json_encode(['wp' => $wp]);    
 
 } elseif ($type === 'themes') {
+    $url = $_GET['url'];
     $themes = get_themes($url);
     echo json_encode(['themes' => $themes]);
 
 } elseif ($type === 'plugins') {
+    $url = $_GET['url'];
     $plugins = get_plugins($url);
     echo json_encode(['plugins' => $plugins]);
     
 } elseif ($type === 'top-themes') {
-    $themes = get_top_themes();
+    $quantity = $_GET['quantity'];
+    $page = $_GET['page'];
+    $themes = get_top_themes($quantity, $page);
     echo json_encode(['themes' => $themes]);
 
 } elseif ($type === 'top-plugins') {
-    $plugins = get_top_plugins();
+    $quantity = $_GET['quantity'];
+    $page = $_GET['page'];
+    $plugins = get_top_plugins($quantity, $page);
     echo json_encode(['plugins' => $plugins]);
 }
 ?>
