4e14b395ff92bb98b62de6fc4055d380049221d7
Adria Blancafort <adriablancafort@gmail.com>
2024-07-08 17:51:45 +0200

    improved detection accuracy

--- find_wp.php
+++ find_wp.php
@@ -1,11 +1,20 @@
 <?php
 
 // Returns true if any of the links contain '/wp-content/'
-function find_wp($links) 
+function find_wp($links, $url) 
 {
+    $parsedUrl = parse_url($url);
+    $urlHost = $parsedUrl['host'];
+
     foreach ($links as $link) {
         if (strpos($link, '/wp-content/') !== false) {
-            return true;
+
+            $parsedLink = parse_url($link);
+            $linkHost = $parsedLink['host'];
+
+            if ($urlHost === $linkHost) {
+                return true;
+            }
         }
     }
     
