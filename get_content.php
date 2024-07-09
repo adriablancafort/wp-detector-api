59b9a3d78bc2403cfafd81af5a3fb6ab2f4fe52b
Adria Blancafort <adriablancafort@gmail.com>
2024-07-09 11:28:19 +0200

    return null if a timeout of 10 seconds is exceeded

--- get_content.php
+++ get_content.php
@@ -1,15 +1,15 @@
 <?php
 
 // Returns the content content of a given url
-function get_content($url) 
+function get_content($url)
 {
 	$headers = array(
-	   "Connection: keep-alive",
-	   "Upgrade-Insecure-Requests: 1",
-	   "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-	   "Accept-Language: en,es-ES;q=0.9,es;q=0.8",
+		"Connection: keep-alive",
+		"Upgrade-Insecure-Requests: 1",
+		"Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+		"Accept-Language: en,es-ES;q=0.9,es;q=0.8",
 	);
-	
+
 	$agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/117.0';
 
 	$cookieFilePath = __DIR__ . '/cookie.txt';
@@ -23,24 +23,24 @@ function get_content($url)
 	curl_setopt($ch, CURLOPT_ENCODING, '');
 	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
 	curl_setopt($ch, CURLOPT_AUTOREFERER, true);
-	curl_setopt($ch, CURLOPT_CONNECTTIMEOUT , 60); 
-	curl_setopt($ch, CURLOPT_TIMEOUT, 60); 
-	curl_setopt($ch, CURLOPT_MAXREDIRS, 5); 
+	curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 60);
+	curl_setopt($ch, CURLOPT_TIMEOUT, 10); // Set timeout to 10 seconds
+	curl_setopt($ch, CURLOPT_MAXREDIRS, 5);
 	curl_setopt($ch, CURLOPT_COOKIE, 'consent=yes');
 	curl_setopt($ch, CURLOPT_COOKIEFILE, $cookieFilePath);
 	curl_setopt($ch, CURLOPT_COOKIEJAR, $cookieFilePath);
-	curl_setopt($ch, CURLOPT_URL,$url);
-	$content=curl_exec($ch);
-	
-	// Check for cURL errors
-	if (curl_errno($ch)) {
+	curl_setopt($ch, CURLOPT_URL, $url);
+	$content = curl_exec($ch);
+
+	// Check for cURL errors or timeout
+	if (curl_errno($ch) || $content === false) {
 		$error_message = curl_error($ch);
 		curl_close($ch);
-		return '';
+		return null; // Return null on error or timeout
 	}
 
 	curl_close($ch);
-	
+
 	return $content;
 }
 ?>
