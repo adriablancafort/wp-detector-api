3dcee2a6eb764201a919edbe8ce68e5f55c2a139
Adria Blancafort <adriablancafort@gmail.com>
2024-07-11 16:03:41 +0200

    find wp compare names, not hosts www.example.com == example.com

--- find_wp.php
+++ find_wp.php
@@ -3,21 +3,31 @@
 // Returns true if any of the links contain '/wp-content/'
 function find_wp($links, $url) 
 {
-    $parsedUrl = parse_url($url);
-    $urlHost = $parsedUrl['host'];
+    $urlName = get_url_name($url);
 
     foreach ($links as $link) {
         if (strpos($link, '/wp-content/') !== false) {
 
-            $parsedLink = parse_url($link);
-            $linkHost = $parsedLink['host'];
+            $linkName = get_url_name($link);
 
-            if ($urlHost === $linkHost) {
+            if ($urlName === $linkName) {
                 return true;
             }
         }
     }
     
     return false;
+}
+
+// Returns the name of the website from the URL. https://www.example.com -> example.com
+function get_url_name($url)
+{
+    $parsedUrl = parse_url($url);
+    $urlHost = $parsedUrl['host'];
+
+    // Remove 'www.' from the host if present
+    $urlName = preg_replace('/^www\./', '', $urlHost);
+
+    return $urlName;
 }
 ?>
