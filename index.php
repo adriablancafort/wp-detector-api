2da0177ac7b8252d11b9b75d222b0c92d3806b99
Adria Blancafort <adriablancafort@gmail.com>
2024-07-19 11:14:33 +0200

    remove url parameters and refactor url cleanning

--- index.php
+++ index.php
@@ -8,23 +8,36 @@ require_once 'get_themes.php';
 require_once 'get_plugins.php';
 require_once 'get_websites.php';
 
+function cleanUrl($url)
+{
+    $parsedUrl = parse_url($url);
+    $cleanUrl = $parsedUrl['scheme'] . '://' . $parsedUrl['host'];
+    // Append the path to the url if it is not empty
+    $path = $parsedUrl['path'];
+    if (isset($path)) {
+        $cleanUrl .= $path;
+    }
+    return $cleanUrl;
+}
+
 $type = $_GET['type'];
+$url = $_GET['url']; // Store multiple pages of the same website as a single website in the database. Analyze multiple pages of the same website for every query?
+if (isset($url)) {
+    $cleanUrl = cleanUrl($url);
+}
 
 if ($type === 'wp') {
-    $url = $_GET['url']; // Store multiple pages of the same website as a single website in the database. Analyze multiple pages of the same website for every query?
-    $wp = get_wp($url);    
+    $wp = get_wp($cleanUrl);    
     echo json_encode(['wp' => $wp]);    
 
 } elseif ($type === 'themes') {
-    $url = $_GET['url'];
-    $themes = get_themes($url);
+    $themes = get_themes($cleanUrl);
     echo json_encode(['themes' => $themes]);
 
 } elseif ($type === 'plugins') {
-    $url = $_GET['url'];
-    $plugins = get_plugins($url);
+    $plugins = get_plugins($cleanUrl);
     echo json_encode(['plugins' => $plugins]);
-    
+
 } elseif ($type === 'top-themes') {
     $quantity = $_GET['quantity'];
     $page = $_GET['page'];
@@ -37,7 +50,7 @@ if ($type === 'wp') {
     $plugins = get_top_plugins($quantity, $page);
     echo json_encode(['plugins' => $plugins]);
 }
-
+/*
 elseif ($type === 'websites-themes') {
     $websitesThemes = get_websites_themes();
     echo json_encode(['websitesThemes' => $websitesThemes]);
@@ -47,4 +60,5 @@ elseif ($type === 'websites-plugins') {
     $websitesPlugins = get_websites_plugins();
     echo json_encode(['websitesPlugins' => $websitesPlugins]);
 }
+*/
 ?>
